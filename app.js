281165b2b7a33c3bd664bc100c0b997d42ed0deb
Chirag Sardana <macbookair@MacBook-Air.local>
2022-06-26 19:51:33 +0530

    Some Confirguration

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 // This is App.js 
 const express = require("express");
 const mongoose = require('./db/connect');
+const cors = require("cors");
 const res = require("express/lib/response");
 const app = express(); // express function and it return an app fn
 app.use(express.static("public"));
 require("dotenv").config();
+app.use(express.json()); // {key:value}
+app.use(express.urlencoded()); // key=value&key=value
 
 
 const server = app.listen(process.env.PORT || 1234, (err) => {
